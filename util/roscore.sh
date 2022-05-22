052fa40f93e58ea692163bc583161ed992b0a6ef
Ameer Hamza <ahamza.bese16seecs@seecs.edu.pk>
2022-05-22 10:13:25 +0100

    Display fix

--- util/roscore.sh
+++ util/roscore.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 export TURTLEBOT3_MODEL=burger
+
+Xvfb :1 -screen 0 1600x1200x16  &
+
+
 ROS_MASTER_URI="0.0.0.0"
 source /opt/ros/melodic/setup.bash
 source /content/mapless/catkin_ws/devel/setup.bash
-roslaunch turtlebot3_gazebo turtlebot3_stage_2.launch
+
+DISPLAY=:1.0 roslaunch turtlebot3_gazebo turtlebot3_stage_2.launch
