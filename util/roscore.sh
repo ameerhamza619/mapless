d531bf36a69ba659e442467389de2ce3b1e3a4c0
Ameer Hamza <ahamza.bese16seecs@seecs.edu.pk>
2022-05-23 12:38:52 +0100

    Update roscore.sh

--- util/roscore.sh
+++ util/roscore.sh
@@ -9,4 +9,4 @@ ROS_MASTER_URI="0.0.0.0"
 source /opt/ros/melodic/setup.bash
 source /content/mapless/catkin_ws/devel/setup.bash
 
-DISPLAY=:1.0 roslaunch turtlebot3_gazebo train_env2.launch
+DISPLAY=:1.0 roslaunch turtlebot3_gazebo turtlebot3_stage_2.launch
